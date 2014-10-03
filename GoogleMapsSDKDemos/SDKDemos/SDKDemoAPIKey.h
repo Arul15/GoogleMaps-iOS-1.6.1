5a221181b68f43894243cfbc3bbfa43400945541
Arul15 <ntlbusinessapp@sixcroresapp.local>
2014-10-03 18:36:37 +0530

    GoogleMaps-iOS-1.6.1
    GoogleMaps-iOS-1.6.1

---
+++ GoogleMapsSDKDemos/SDKDemos/SDKDemoAPIKey.h
@@ -0,0 +1,8 @@
+/**
+ * To use GoogleMapsSDKDemos, please register an APIKey for your application
+ * and set it here. Please see the README for more information. Your APIKey
+ * should be kept private.
+ */
+
+static NSString *const kAPIKey = @"https://maps.googleapis.com/maps/api/place/autocomplete/json?input=%@&types=geocode&components=country:in&location=13.024402679160448,80.20384004076353&radius=500&language=en&sensor=true&key=AIzaSyC2Jo0Io2rJ70NzeRuTf5UyVWGX5UAGxj0";
+
